476cda52c113a12b0b4f7c5f7f4ec48152ff2fb1
Adriel Arocha <adrielarochaoronoz@gmail.com>
2021-09-08 20:58:09 +0000

    numeros primitivos

---
+++ 02-fundamentos-tiposObjetos/assets/js/app.js
@@ -0,0 +1,67 @@
+//vamos a ver los tipos d edatos primitivos
+let nombre = 'peter Parker'
+
+console.log(nombre);
+
+
+
+// no se puede volver a inicializar la variable con let
+//! let nombre = 'ben Parker'
+
+//! console.log(nombre)
+
+//? asignar nuevos valores
+
+nombre = 'tia may'
+
+
+//? comprobar tipo de valor
+console.log( typeof nombre);//string
+
+let esMarvel =false;
+
+console.log(typeof esMarvel);//boolean
+
+
+let edad = 33;
+console.log(typeof edad);//number
+
+
+let superPoder;
+
+console.log(typeof superPoder);//undefined
+
+let nulli =null;
+
+console.log(nulli);//pues es null, esperaba object
+
+let symbol = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(typeof symbol)//symbol
+console.log(symbol)
+
+console.log(symbol===symbol2);
+
+
+
+
+
+//palabras reservadas y nomreb de variables
+
+
+
+
+
+
+//array
+
+//mas sobre array
+
+//objetos
+
+//mas sobre los objetos
+
+//funciones y funciones flecha
+
+//retorno de funciones
