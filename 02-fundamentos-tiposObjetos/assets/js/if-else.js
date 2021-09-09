76df9fd1a0648cc256585e088600425536241c04
Adriel Arocha <adrielarochaoronoz@gmail.com>
2021-09-09 09:31:04 +0000

    ejercicio if-else

--- 02-fundamentos-tiposObjetos/assets/js/if-else.js
+++ 02-fundamentos-tiposObjetos/assets/js/if-else.js
@@ -12,13 +12,31 @@ const hoy = new Date();
 
 let dia = hoy.getDay();
 
+
 if(dia === 0){
     console.log('es domingo')
 }else{
     console.log('no es domingo', dia)
-}
+};
 
 
-console.log('fin programa')
+console.log('fin programa');
 //? valores iguales ==
 //? valores estrictamente iguales === , esto comprueba el valor y el tipo de dato
+//ejercicio
+const diaSemana =['domingo','lunes','martes','miercoles','jueves','viernes','sabado'];
+
+console.log(diaSemana[dia]);
+
+const diasSemanaObjeto={
+    0:'domingo',
+    1:'lunes',
+    2:'martes',
+    3:'miercoles',
+    4:'jueves',
+    5:'viernes',
+    6:'sabado'
+};
+
+console.log('dia de la semana', diasSemanaObjeto[dia])
+
