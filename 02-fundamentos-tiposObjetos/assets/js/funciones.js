fdd79246dbc8e7396e34f73b465543e0953598c9
Adriel Arocha <adrielarochaoronoz@gmail.com>
2021-09-09 07:53:05 +0000

    vemos la definicionde las funciones, los retornos de las mismas y funciones de flecha

--- 02-fundamentos-tiposObjetos/assets/js/funciones.js
+++ 02-fundamentos-tiposObjetos/assets/js/funciones.js
@@ -43,3 +43,38 @@ const saludarFlecha = ()=>{
 
 
 saludarFlecha();
+
+
+//retorno de las funciones
+//todas las funciones devuelven algo
+//! si no esta definido devolvera undefined
+
+//establecemos el retorno de la funcion con la palbra reservada return
+//el return termina la ejecucion de la funcion
+
+const devuelveUnObjetoliteral = ()=>{
+    console.log('te mando el objeto');
+    return {
+        nombre:'adriel',
+        edad:34
+    }
+}
+
+const objeto =devuelveUnObjetoliteral()
+console.log(objeto);
+
+
+//si solo vamos a hacer una instruccion
+
+const queWaymiFuncion = (a,b) => a+b  //nos devuelve la suma de a y b
+
+//ejercicio de transformar una funcion tradicional a una funcion flecha de una sola linea
+
+//?funcion a transformar
+function getAleatorio(){
+    return Math.random()
+}
+
+//?funcion transformada
+const getAleatorio2 = () => Math.random();
+
